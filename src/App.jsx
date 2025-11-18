7cf07410be74b4296a7d73abf693a703f04cc0a8
darkogolovic <golovicdarko@gmail.com>
2025-11-18 15:08:42 +0100

    router setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,6 @@
-import { Toaster } from 'react-hot-toast';
-import SignIn from './pages/sign-in/sign-in';
-import SignUp from './pages/sign-up/sign-up';
 
 const App = () => {
-  return (
-    <>
-    
-    <SignUp />
-    <Toaster  
-        position='top-center'
-        toastOptions={{
-            succes: {duration : 2000},
-            error: {duration : 2000}
-        }}
-    />
-    </>
-  );
+  return null
 }
 
 
